48d154ea1076fb523780ead93b8fd94a0202f2ee
arthineruturi <108206838+arthineruturi@users.noreply.github.com>
2023-12-08 00:37:50 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'seproj.mysql.database.azure.com');
-   define('DB_USERNAME', 'arthi');
-   define('DB_PASSWORD', 'Project123456');
+   define('DB_SERVER', 'arthiazure.mysql.database.azure.com');
+   define('DB_USERNAME', 'arthisri');
+   define('DB_PASSWORD', 'A1@azure');
    define('DB_DATABASE', 'testing');
    $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
